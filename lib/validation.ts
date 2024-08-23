f98f52fc9557654e699b66409c6f5cec87d53217
Charlie Palmer <charlie.palmer@bluefrontier.co.uk>
2024-08-23 23:43:22 +0100

    feat: implementing create user functionality

--- lib/validation.ts
+++ lib/validation.ts
@@ -4,7 +4,7 @@ export const UserFormValidation = z.object({
     name: z
         .string()
         .min(2, "Name must be at least 2 characters.")
-        .max(2, "Name must be at most 15 characters."),
+        .max(15, "Name must be at most 15 characters."),
     email: z.string().email("Invalid email address."),
     phone: z
         .string()
