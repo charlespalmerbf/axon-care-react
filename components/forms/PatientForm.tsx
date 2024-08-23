83f4e59339d18c62e21ae3a348ac74ef8d170483
Charlie Palmer <charlie.palmer@bluefrontier.co.uk>
2024-08-23 12:43:21 +0100

    feat: implementing form validation using zod

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -1,12 +1,15 @@
 "use client";
 
+import { useState } from "react";
+
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
-import { Button } from "@/components/ui/button";
 import { Form } from "@/components/ui/form";
 import CustomFormField from "../CustomFormField";
+import SubmitButton from "../SubmitButton";
+import { UserFormValidation } from "@/lib/validation";
 
 export enum FormFieldType {
     INPUT = "input",
@@ -18,21 +21,19 @@ export enum FormFieldType {
     SKELETON = "skeleton",
 }
 
-const formSchema = z.object({
-    username: z.string().min(2, {
-        message: "Username must be at least 2 characters.",
-    }),
-});
-
 const PatientForm = () => {
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
+    const [isLoading, setIsLoading] = useState(false);
+
+    const form = useForm<z.infer<typeof UserFormValidation>>({
+        resolver: zodResolver(UserFormValidation),
         defaultValues: {
-            username: "",
+            name: "",
+            email: "",
+            phone: "",
         },
     });
 
-    function onSubmit(values: z.infer<typeof formSchema>) {
+    function onSubmit(values: z.infer<typeof UserFormValidation>) {
         console.log(values);
     }
 
@@ -73,7 +74,7 @@ const PatientForm = () => {
                     label="Phone number"
                     placeholder="(+44) 7538 705411"
                 />
-                <Button type="submit">Submit</Button>
+                <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
             </form>
         </Form>
     );
