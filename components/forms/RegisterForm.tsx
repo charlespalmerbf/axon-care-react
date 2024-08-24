cbc454df76e2a384a657f22711ed9dc54d0da07e
Charlie Palmer <charlie.palmer@bluefrontier.co.uk>
2024-08-24 22:23:51 +0100

    feat: adding insurance fields to register form

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -204,6 +204,23 @@ const RegisterForm = ({ user }: { user: User }) => {
                     ))}
                 </CustomFormField>
 
+                <div className="flex flex-col gap-6 xl:flex-row">
+                    <CustomFormField
+                        fieldType={FormFieldType.INPUT}
+                        control={form.control}
+                        name="insuranceProvider"
+                        label="Insurance provider"
+                        placeholder="BlueCross BlueShield"
+                    />
+                    <CustomFormField
+                        fieldType={FormFieldType.INPUT}
+                        control={form.control}
+                        name="insurancePolicyNumber"
+                        label="Insurance policy number"
+                        placeholder="#ABC123456"
+                    />
+                </div>
+
                 <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
             </form>
         </Form>
